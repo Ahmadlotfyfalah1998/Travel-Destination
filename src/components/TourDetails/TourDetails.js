8fd65b934852fe33a83ab54f1f205a7b31f33022
Ahmadlotfyfalah1998 <ahmad.lotfy.falah@gmail.com>
2023-04-06 08:36:45 +0000

    class17..

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -1,6 +1,7 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 function TourDetails(props){
 const {id} =useParams()
 
@@ -10,7 +11,7 @@ let a =props.tour.filter(index => index.id==id)
     console.log(a)
     
     
-
+let [ReadMore,setReadMore]=useState(false)
 return (
 <>
 <Header/>
@@ -20,7 +21,27 @@ return (
 <h1 className="info">{a[0].name}</h1>
 <h2 className="info">{a[0].price}</h2>
 <img  id="imgDetails"  src={a[0].image}/>
-<h5>{a[0].info}</h5>
+<div>
+{
+ReadMore? <>
+<p>{a[0].info}</p>
+<button  onClick={()=>setReadMore(false)}>show less</button>
+
+
+
+</>
+:
+<>
+<p>{(a[0].info).substring(0,150)}</p>
+<button  onClick={()=>setReadMore(true)}>show more</button>
+
+
+</>
+}
+</div>
+
+
+
 
 </div>
 
